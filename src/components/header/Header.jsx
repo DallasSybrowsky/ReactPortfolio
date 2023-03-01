a691049239b38b3ee327f225e4d7d23c85f2ae49
Dallas Sybrowsky <dallas.sybrowsky@gmail.com>
2023-03-01 14:04:20 -0800

    Updated files, color palette, and added transitions

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,9 +17,9 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        <a href="#contact" className="scroll__down">
+        {/* <a href="#contact" className="scroll__down">
           Scroll Down
-        </a>
+        </a> */}
       </div>
     </header>
   );
