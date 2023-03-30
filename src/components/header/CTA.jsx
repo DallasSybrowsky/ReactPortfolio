c511bbfcafbcf56d743b7a475dbe2c4cabb65eae
Dallas Sybrowsky <dallas.sybrowsky@gmail.com>
2023-03-30 07:39:33 -0700

    Updated resume link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../../assets/DSybrowskyResume.pdf";
+import resume from "../../assets/DSybrowskyDeveloperResume.pdf";
 
 const CTA = () => {
   return (
